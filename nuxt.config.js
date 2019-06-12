424555b52d7e2acdb8de9aadca52dae588012b78
eduard <mikroffon@gmail.com>
2019-06-12 22:43:11 +0300

    fixed metrics

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,14 +28,18 @@ module.exports = {
   serverMiddleware: ['~/api/telegram.js'],
   loading: { color: '#3B8070' },
   modules: [
-    '@nuxtjs/sitemap', '@nuxtjs/robots', '@nuxtjs/yandex-metrika', {
-      id: '54037297',
-      webvisor: true,
-      clickmap: true,
-      useCDN: false,
-      trackLinks: true,
-      accurateTrackBounce: true
-    }],
+    '@nuxtjs/sitemap', '@nuxtjs/robots', [
+      '@nuxtjs/yandex-metrika',
+      {
+        id: '54037297',
+        webvisor: true,
+        clickmap: true,
+        useCDN: false,
+        trackLinks: true,
+        accurateTrackBounce: true
+      }
+    ]
+  ],
   sitemap: {
     path: '/sitemap.xml',
     hostname: 'https://doit.kiev.ua',
