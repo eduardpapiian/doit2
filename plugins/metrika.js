4361a98c3376e7c04a67a1446fe55316ccbde0fc
eduard <mikroffon@gmail.com>
2019-06-12 22:27:30 +0300

    metrika

--- plugins/metrika.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import VueYandexMetrika from 'vue-yandex-metrika'
-
-Vue.use(VueYandexMetrika, {
-  id: 54037297,
-  env: process.env.NODE_ENV,
-  options: { webvisor: true, clickmap: true, trackLinks: true, accurateTrackBounce: true }
-  // other options
-})
