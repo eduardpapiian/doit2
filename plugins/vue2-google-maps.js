69204f900b85753c7dc9be3df3f46b7620f9feb5
eduardpapiian <mikroffon@gmail.com>
2019-12-10 16:40:14 +0200

    added accii kpi

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -3,6 +3,7 @@ import * as VueGoogleMaps from '~/node_modules/vue2-google-maps/src/main'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyBBSngG4LKCW24T8e1G0P5bNK47op6G-c0'
+    // key: 'AIzaSyBBSngG4LKCW24T8e1G0P5bNK47op6G-c0'
+    key: 'AIzaSyDPazWr5u_lq1O0EPkQ3JXM3e69agUT3F4'
   }
 })
